06ac83628acf5de2061b339924e0c26142c1e88f
sabio <armandojpr@yahoo.com>
2018-07-15 21:55:24 -0500

    Se sube ejemplo de controller y service

---
+++ src/main/java/com/udg/prueba/service/Operacion.java
@@ -0,0 +1,13 @@
+package com.udg.prueba.service;
+
+public class Operacion {
+
+    public Integer suma(Integer a, Integer b){
+        return a+b;
+    }
+    
+    public Integer resta(Integer a, Integer b){
+        return a-b;
+    }
+    
+}
---
+++ src/test/java/test/OperacionTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author armando
+ */
+public class OperacionTest extends TestCase {
+    
+    public OperacionTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    public void testSuma(){
+        
+    }
+}
